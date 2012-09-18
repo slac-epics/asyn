22ebfd8b9f99d3f45822770bba53a1564d048471
Ernest Williams <ernesto@slac.stanford.edu>
2012-09-18 15:59:54 +0000

    use EPICS R3-14-12 style template

--- configure/makeIncludeDbd.pl
+++ configure/makeIncludeDbd.pl
@@ -9,7 +9,7 @@
 # in file LICENSE that is included with this distribution. 
 #*************************************************************************
 #
-# $Id: makeIncludeDbd.pl,v 1.1.1.4 2011/02/24 06:01:19 ernesto Exp $
+# $Id: makeIncludeDbd.pl,v 1.7 2012/06/25 14:30:26 ernesto Exp $
 
 use File::Basename;
 
